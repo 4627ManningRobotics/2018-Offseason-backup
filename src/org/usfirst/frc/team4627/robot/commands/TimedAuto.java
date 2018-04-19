a6f8958e365ee9ef6bfd05bf06b31dce56c8be87
JoeTodes <joe.eng.wilson@gmail.com>
2018-04-19 14:49:05 -0600

    center auto

--- src/org/usfirst/frc/team4627/robot/commands/TimedAuto.java
+++ src/org/usfirst/frc/team4627/robot/commands/TimedAuto.java
@@ -14,9 +14,9 @@ public class TimedAuto extends CommandGroup {
     	addSequential(new DriveForward(0.5, 0.3));
     	//addSequential(new  TurnToAngleFMS(45,  0.2,  0.5,  3));
     	addSequential(new TurnToAngleTimeFMS(0.4, 0.6)); // turn
-    	addSequential(new DriveForwardFMS(0.7, 0.95, 0.2));
+    	addSequential(new DriveForwardFMS(0.7, 0.80, 0.2));
     	//addSequential(new  TurnToAngleFMS(-47,  0.2,  0.5,  3));
-    	addSequential(new TurnToAngleTimeFMS(0.45, -0.45)); // turn
+    	addSequential(new TurnToAngleTimeFMS(0.4, -0.45)); // turn
     	addSequential(new  DriveForwardFMS(0.3, 1, .1));
     	/*
     	addSequential(new DriveForward(0.25, 0.25, 4)); //1s
